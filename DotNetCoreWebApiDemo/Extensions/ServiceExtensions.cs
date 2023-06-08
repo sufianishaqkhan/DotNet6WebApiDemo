9d5833052ec941ab0eaef50519ce76c2cd362e8e
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-08 19:14:24 +0500

    Basic setup.

---
+++ DotNetCoreWebApiDemo/Extensions/ServiceExtensions.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace DotNetCoreWebApiDemo.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //CORS Policy
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+
+        //Authentication Policy
+        public static void ConfigureSecurity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration.GetValue<string>("Jwt:Issuer"),
+                    ValidAudience = configuration.GetValue<string>("Jwt:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("Jwt:Key"))),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+
+        //Swagger Gen Security Configuration
+        public static void SwaggerGenSecurity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = configuration["Application_Title"],
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Scheme = "bearer",
+                    Description = configuration.GetValue<string>("Jwt:Description")
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                   Type = ReferenceType.SecurityScheme,
+                                   Id = "Bearer"
+                                }
+                            },
+                        new string[]
+                        {
+                        }
+                    }
+                });
+            });
+        }
+
+        public static void ConfigureDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            // ************************** Services Configuration *****************************
+            //services.AddScoped<IExceptionService, ExceptionService>();
+            //services.AddScoped<ISerializeService, SerializeService>();
+            //services.AddScoped<ICustomLogger, CustomLogger>();
+            //services.AddScoped<IDeserializeService, DeserializeService>();
+            //services.AddScoped<IAuthorizeRepository, AuthorizeRepository>();
+
+            //// ************************** Servivces Configuration STORE *****************************
+            //services.AddTransient<IUsersService, UsersService>();
+            //services.AddScoped<IStoreUnitOfWork, StoreUnitOfWork>();
+
+
+            //// ************************** Database Configuration *****************************
+            //services.AddDbContext<NTPEP_DATAMINE3Context>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("NTPEPConnection"),
+            //sqlServerOptions => sqlServerOptions.CommandTimeout(600)));
+        }
+
+        //ApiController model state verification skip
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+        }
+    }
+}
---
+++ DotNetCoreWebApiDemo/Program.cs
@@ -0,0 +1,46 @@
+using DotNetCoreWebApiDemo.Extensions;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureDependencies(builder.Configuration);
+builder.Services.ConfigureServices();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.SwaggerGenSecurity(builder.Configuration);
+builder.Services.ConfigureSecurity(builder.Configuration);
+builder.Services.AddAuthorization();
+
+// Configure Logging.
+builder.Host.UseSerilog((hostContect, services, configuration) =>
+{
+    configuration.WriteTo.File("CustomLog/log-.txt", rollingInterval: RollingInterval.Day, outputTemplate:
+        "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.DefaultModelsExpandDepth(-1);
+});
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
