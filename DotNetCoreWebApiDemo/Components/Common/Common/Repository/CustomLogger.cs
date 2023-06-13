e129c19fefdeecfe5b1cadc397a35e0000cc98f6
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-13 19:45:05 +0500

    Logger and exception service.

--- DotNetCoreWebApiDemo/Components/Common/Common/Repository/CustomLogger.cs
+++ DotNetCoreWebApiDemo/Components/Common/Common/Repository/CustomLogger.cs
@@ -9,20 +9,20 @@ namespace Common.Repository
 {
     public class CustomLogger : ICustomLogger
     {
-        private readonly ILogger<CustomLogger> _logger;
-        public CustomLogger(ILogger<CustomLogger> logger)
-        {
-            _logger = logger;
-        }
-        public void LogInfo(string method, string message, string? parameters = null)
-        {
-            _logger.LogInformation("Method: " + method + " Parameters: " + parameters + " Message: " + message);
-        }
-        public void LogError(string method, string exceptionMessage, string? parameters = null)
-        {
-            _logger.LogInformation("**************************" + method + "'s Exception ***********************************");
-            _logger.LogError("Method: " + method + " Parameters: " + parameters + " Exception: " + exceptionMessage);
-            _logger.LogInformation("**************************" + method + "'s Exception End ***********************************");
-        }
+        //private readonly ILogger<CustomLogger> _logger;
+        //public CustomLogger(ILogger<CustomLogger> logger)
+        //{
+        //    _logger = logger;
+        //}
+        //public void LogInfo(string method, string message, string? parameters = null)
+        //{
+        //    _logger.LogInformation("Method: " + method + " Parameters: " + parameters + " Message: " + message);
+        //}
+        //public void LogError(string method, string exceptionMessage, string? parameters = null)
+        //{
+        //    _logger.LogInformation("**************************" + method + "'s Exception ***********************************");
+        //    _logger.LogError("Method: " + method + " Parameters: " + parameters + " Exception: " + exceptionMessage);
+        //    _logger.LogInformation("**************************" + method + "'s Exception End ***********************************");
+        //}
     }
 }
--- DotNetCoreWebApiDemo/Components/Common/Common/Repository/ExceptionService.cs
+++ DotNetCoreWebApiDemo/Components/Common/Common/Repository/ExceptionService.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Interface;
+using Entities.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
