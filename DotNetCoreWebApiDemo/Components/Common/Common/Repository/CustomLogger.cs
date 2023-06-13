67f7285509af36a36df1e5386f9a142d1b6645e4
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-13 19:46:51 +0500

    Logger deactivated to pass build.

--- DotNetCoreWebApiDemo/Components/Common/Common/Repository/CustomLogger.cs
+++ DotNetCoreWebApiDemo/Components/Common/Common/Repository/CustomLogger.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace Common.Repository
 {
-    public class CustomLogger : ICustomLogger
-    {
-        //private readonly ILogger<CustomLogger> _logger;
-        //public CustomLogger(ILogger<CustomLogger> logger)
-        //{
-        //    _logger = logger;
-        //}
-        //public void LogInfo(string method, string message, string? parameters = null)
-        //{
-        //    _logger.LogInformation("Method: " + method + " Parameters: " + parameters + " Message: " + message);
-        //}
-        //public void LogError(string method, string exceptionMessage, string? parameters = null)
-        //{
-        //    _logger.LogInformation("**************************" + method + "'s Exception ***********************************");
-        //    _logger.LogError("Method: " + method + " Parameters: " + parameters + " Exception: " + exceptionMessage);
-        //    _logger.LogInformation("**************************" + method + "'s Exception End ***********************************");
-        //}
-    }
+    //public class CustomLogger : ICustomLogger
+    //{
+    //    private readonly ILogger<CustomLogger> _logger;
+    //    public CustomLogger(ILogger<CustomLogger> logger)
+    //    {
+    //        _logger = logger;
+    //    }
+    //    public void LogInfo(string method, string message, string? parameters = null)
+    //    {
+    //        _logger.LogInformation("Method: " + method + " Parameters: " + parameters + " Message: " + message);
+    //    }
+    //    public void LogError(string method, string exceptionMessage, string? parameters = null)
+    //    {
+    //        _logger.LogInformation("**************************" + method + "'s Exception ***********************************");
+    //        _logger.LogError("Method: " + method + " Parameters: " + parameters + " Exception: " + exceptionMessage);
+    //        _logger.LogInformation("**************************" + method + "'s Exception End ***********************************");
+    //    }
+    //}
 }
