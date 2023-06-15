dde4905d8d5d0bb71f3a3095c42c4d41a1c157c0
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-15 18:50:22 +0500

    Unit of work added.

---
+++ Components/DataAccess/Repository/StoreUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Common.Interface;
+using DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class StoreUnitOfWork : IStoreUnitOfWork
+    {
+        private readonly DbContext? _context;
+        private readonly ICustomLogger _logger;
+        public StoreUnitOfWork(DbContext context, ICustomLogger logger)
+        {
+            _context = context;
+            _logger = logger;
+
+            Users = new UsersRepository(_context, _logger);
+
+        }
+        public IUsersRepository Users { get; private set; }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
