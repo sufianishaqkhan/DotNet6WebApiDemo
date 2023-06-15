624ae68acdd6f40b0bee9606c5abb81a9545c578
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-15 18:55:12 +0500

    Generic repository added.

---
+++ Components/DataAccess/Interface/IAuthorizeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interface
+{
+    public interface IAuthorizeRepository
+    {
+        bool AuthenticateUser(string pUserName, string pPassword);
+        Task<AuthenticationModel> CreateToken(string userName);
+    }
+}
